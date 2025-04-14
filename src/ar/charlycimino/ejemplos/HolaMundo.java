38c5253734a5decb41954d316d8c33f012a7b10b
yukiel <ytorres@arkusnexus.com>
2025-04-14 10:51:02 -0700

    Add second message

--- src/ar/charlycimino/ejemplos/HolaMundo.java
+++ src/ar/charlycimino/ejemplos/HolaMundo.java
@@ -11,6 +11,7 @@ public class HolaMundo {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Hola mun2");
         System.out.println("Adios mundo");
     }
 
