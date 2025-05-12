9e90e829b8a6e1b37fab637fac7f316b503f51e8
yukiel <ytorres@arkusnexus.com>
2025-05-12 12:27:46 -0700

    linesDe

--- src/ar/charlycimino/ejemplos/HolaMundo.java
+++ src/ar/charlycimino/ejemplos/HolaMundo.java
@@ -13,10 +13,7 @@ public class HolaMundo {
         System.out.println("Hola mundo");
         System.out.println("Hola mun2");
         System.out.println("Adios mundo");
-        System.out.println("Adios mundo2");
-        System.out.println("Adios mundo3");
-        System.out.println("Adios mundo4");
-        System.out.println("Adios mundo5");
+     
     }
 
 }
